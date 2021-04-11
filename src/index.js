19cd2287d1bff622b40a250514f54d5e0352a1a1
У М <umalat.magma@gmail.com>
2021-04-11 22:58:01 +0300

    Прокинул стор через Context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import bridge from '@vkontakte/vk-bridge';
 import App from './App';
-
+import { StoreContext, Store } from './store/store';
 // Init VK  Mini App
 bridge.send('VKWebAppInit');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StoreContext.Provider value={new Store()}>
+    <App />
+  </StoreContext.Provider>,
+  document.getElementById('root')
+);
 if (process.env.NODE_ENV === 'development') {
   import('./eruda').then(({ default: eruda }) => {}); //runtime download
 }
