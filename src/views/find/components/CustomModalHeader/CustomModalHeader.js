62a4d665295579dc7ead936d6a12516ace4b3606
У М <umalat.magma@gmail.com>
2021-04-13 01:41:49 +0300

    Собрал кастомный ModalHeader, для сокращения кода

---
+++ src/views/find/components/CustomModalHeader/CustomModalHeader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ModalPageHeader, PanelHeaderButton, VKCOM, IOS, ANDROID } from '@vkontakte/vkui';
+import { Icon24Cancel, Icon24Dismiss } from '@vkontakte/icons';
+const CustomModalHeader = (props) => (
+  <ModalPageHeader
+    left={
+      (props.platform === ANDROID || props.platform === VKCOM) && (
+        <PanelHeaderButton onClick={props.onCloseClick}>
+          <Icon24Cancel />
+        </PanelHeaderButton>
+      )
+    }
+    right={
+      props.platform === IOS && (
+        <PanelHeaderButton onClick={props.onCloseClick}>
+          <Icon24Dismiss />
+        </PanelHeaderButton>
+      )
+    }
+  >
+    {props.children}
+  </ModalPageHeader>
+);
+
+export default CustomModalHeader;
