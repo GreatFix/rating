45fbd0c918441ffc39642c30999394e60883f29b
У М <umalat.magma@gmail.com>
2021-04-13 01:41:04 +0300

    Создал компонент для отрисовки рейтинга

---
+++ src/views/find/components/Popular/Popular.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Icon24CupOutline } from '@vkontakte/icons';
+
+const COLORS = {
+  neutral: '#99A2AD',
+  positive: '#47C147',
+  negative: '#FF3D50',
+};
+
+const Popular = ({ positive, negative }) => {
+  const popular = getPopular(positive, negative);
+  return (
+    <>
+      <Icon24CupOutline fill={COLORS[popular.rang]} /> {popular.count}
+    </>
+  );
+};
+
+export default Popular;
+
+function getPopular(positive = 0, negative = 0) {
+  const count = Number(positive) - Number(negative);
+  return count === 0
+    ? { count, rang: 'neutral' }
+    : count > 0
+    ? { count, rang: 'positive' }
+    : { count, rang: 'negative' };
+}
