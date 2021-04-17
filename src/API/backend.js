ebba5ec6d3db887a699634f465f4b68fafce7257
У М <umalat.magma@gmail.com>
2021-04-17 20:29:14 +0300

    Вынес Backend и Bridge API в отдельный файл

---
+++ src/API/backend.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+const AXIOS = axios.create({
+  baseURL: 'https://rating-backend.herokuapp.com'
+})
+
+class BACKEND_API {
+  SET_TOKEN(access_token) {
+    AXIOS.defaults.headers.common['Authorization'] = 'Bearer ' + access_token
+  }
+  async GET_USER(config = {}) {
+    return await AXIOS.get('/user', config)
+  }
+  async GET_TARGET(config = {}) {
+    return await AXIOS.get('/target', config)
+  }
+  async GET_TARGETS(config = {}) {
+    return await AXIOS.get('/targets', config)
+  }
+  async POST_AUTH(data = {}, config = {}) {
+    return await AXIOS.post('/auth', data, config)
+  }
+  async POST_FEEDBACK(data = {}, config = {}) {
+    return await AXIOS.post('/feedback', data, config)
+  }
+  async POST_COMMENT(data = {}, config = {}) {
+    return await AXIOS.post('/comment', data, config)
+  }
+  async PUT_FEEDBACK(data = {}, config = {}) {
+    return await AXIOS.put('/feedback', data, config)
+  }
+  async PUT_COMMENT(data = {}, config = {}) {
+    return await AXIOS.put('/comment', data, config)
+  }
+  async DELETE_FEEDBACK(config = {}) {
+    return await AXIOS.delete('/feedback', config)
+  }
+  async DELETE_COMMENT(config = {}) {
+    return await AXIOS.delete('/comment', config)
+  }
+}
+export default new BACKEND_API()
---
+++ src/API/bridge.js
@@ -0,0 +1,78 @@
+import bridge from '@vkontakte/vk-bridge'
+const API_METHOD = 'VKWebAppCallAPIMethod'
+const APP_ID = 7799989
+let ACCESS_TOKEN
+let SCOPE = window.location.search.slice(26).split('&')[0]
+class BRIDGE_API {
+  SET_TOKEN(token, scope) {
+    ACCESS_TOKEN = token
+    SCOPE = scope.length > SCOPE.length ? scope : SCOPE
+  }
+
+  async GET_USER_INFO() {
+    return await bridge.send('VKWebAppGetUserInfo')
+  }
+
+  async GET_AUTH_TOKEN(scope) {
+    return await bridge.send('VKWebAppGetAuthToken', {
+      app_id: APP_ID,
+      scope
+    })
+  }
+
+  async GET_FRIENDS(params = {}) {
+    if (!SCOPE.includes('friends')) return new Error("Don't permission")
+    return await bridge.send(API_METHOD, {
+      method: 'friends.get',
+      params: setV(params)
+    })
+  }
+
+  async GET_GROUPS(params = {}) {
+    if (!SCOPE.includes('groups')) return new Error("Don't permission")
+    return await bridge.send(API_METHOD, {
+      method: 'groups.get',
+      params: setV(params)
+    })
+  }
+  async GET_USERS(params = {}) {
+    return await bridge.send(API_METHOD, {
+      method: 'users.get',
+      params: setV(params)
+    })
+  }
+
+  async SEARCH_GROUPS(params = {}) {
+    return await bridge.send(API_METHOD, {
+      method: 'groups.search',
+      params: setV(params)
+    })
+  }
+
+  async SEARCH_USERS(params = {}) {
+    return await bridge.send(API_METHOD, {
+      method: 'users.search',
+      params: setV(params)
+    })
+  }
+
+  async DB_GET_CITIES(params = {}) {
+    return await bridge.send(API_METHOD, {
+      method: 'database.getCities',
+      params: setV(params)
+    })
+  }
+
+  async DB_GET_COUNTRIES(params = {}) {
+    return await bridge.send(API_METHOD, {
+      method: 'database.getCountries',
+      params: setV(params)
+    })
+  }
+}
+
+export default new BRIDGE_API()
+
+function setV(params) {
+  return { ...params, access_token: ACCESS_TOKEN, v: '5.130' }
+}
